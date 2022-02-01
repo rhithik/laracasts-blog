b739f4715a4f93e1f7240a1f4608160e8b0aff26
Jeff McHale <rhithik@pop-os.localdomain>
2022-01-31 23:14:12 -0500

    learned about collections and cacheing.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,8 +11,14 @@
 <body>
 <?php foreach ($posts as $post) : ?>
 <article>
-    <?= $post; ?>
+    <h1>
+        <a href="/posts/<?= $post->slug; ?>">
+            <?= $post->title; ?>
+        </a>
+    </h1>
+
+    <div><?= $post->excerpt; ?></div>
 </article>
-<?php endforeach ?>;
+<?php endforeach ?>
 </body>
 </html>
